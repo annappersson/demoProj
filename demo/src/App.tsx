51f76c73dee49f9d11d76392e63ac6256b19de6e
annappersson <annasigridmolly@gmail.com>
2026-01-16 14:50:31 +0100

    feat: implement Navigation component and update routing in App

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,12 +3,12 @@ import HomePage from "./pages/HomePage";
 import CitiesPage from "./pages/CitiesPage";
 import ProtectedRoute from "./components/auth/ProtectedRoutes";
 import { ToastContainer } from "react-toastify";
-// import Navigation from "./pages/partials/Navigation";
+import Navigation from "./pages/partials/Navigation";
 
 function App() {
   return (
     <>
-      {/* <Navigation /> */}
+      <Navigation />
       <ToastContainer />
       <Routes>
         <Route path="/" element={<HomePage />} />
--- demo/src/pages/partials/Navigation.tsx
+++ demo/src/pages/partials/Navigation.tsx
@@ -1,56 +1,44 @@
-// import Container from "react-bootstrap/Container";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
-// import { useNavigate } from "react-router";
-// import { toast } from "react-toastify";
-// import { useAuth } from "../../hooks/useAuth";
-// import "../../assets/style/Navigation.scss";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useNavigate } from "react-router";
+import "../../assets/style/Navigation.scss";
 
-// function Navigation() {
-//     const { isAuthenticated, logout } = useAuth();
-//     const navigate = useNavigate();
+function Navigation() {
+  const navigate = useNavigate();
 
-//     const handleLogout = () => {
-//         logout();
-//         toast.success("Du är utloggad");
-//         navigate("/", { replace: true });
-//     };
+  const handleLogout = () => {
+    localStorage.setItem("access_token", "");
+    navigate("/");
+  };
 
-//     return (
-//         <Navbar expand="lg" bg="light">
-//             <Container>
-//                 <Navbar.Brand onClick={() => navigate("/")}>DEMO</Navbar.Brand>
-//                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  return (
+    <Navbar expand="lg" bg="light">
+      <Container>
+        <Navbar.Brand onClick={() => navigate("/")}>DEMO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-//                 <Navbar.Collapse id="basic-navbar-nav">
-//                     <Nav className="me-auto">
-//                         {isAuthenticated && (
-//                             <Nav.Link onClick={() => navigate("/dashboard")}>
-//                                 Cities
-//                             </Nav.Link>
-//                         )}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => navigate("/cities")}>Cities</Nav.Link>
 
-//                         <Nav.Link onClick={() => navigate("/contact")}>
-//                             Kontakt
-//                         </Nav.Link>
+            <Nav.Link onClick={() => navigate("/contact")}>Kontakt</Nav.Link>
 
-//                         {isAuthenticated && (
-//                             <NavDropdown title="Profil" id="basic-nav-dropdown">
-//                                 <NavDropdown.Item onClick={() => navigate("/profile")}>
-//                                     Redigera profil
-//                                 </NavDropdown.Item>
-//                                 <NavDropdown.Divider />
-//                                 <NavDropdown.Item onClick={handleLogout}>
-//                                     Logga ut
-//                                 </NavDropdown.Item>
-//                             </NavDropdown>
-//                         )}
-//                     </Nav>
-//                 </Navbar.Collapse>
-//             </Container>
-//         </Navbar>
-//     );
-// }
+            <NavDropdown title="Profil" id="basic-nav-dropdown">
+              <NavDropdown.Item onClick={() => navigate("/profile")}>
+                Redigera profil
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={handleLogout}>
+                Logga ut
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
-// export default Navigation;
+export default Navigation;
