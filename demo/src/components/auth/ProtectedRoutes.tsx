8e08a730eb89ac04478acc7482c4ca1d7eb15c1a
annappersson <annasigridmolly@gmail.com>
2026-01-16 15:01:54 +0100

    feat: simplify ProtectedRoute logic by removing unnecessary state and effects

--- demo/src/components/auth/ProtectedRoutes.tsx
+++ demo/src/components/auth/ProtectedRoutes.tsx
@@ -1,21 +1,13 @@
 import { Navigate } from "react-router";
 import { useAuth } from "../../context/AuthContext.tsx";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
+// import { useEffect, useState } from "react";
+// import { toast } from "react-toastify";
 import type { JSX } from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const { isAuthenticated } = useAuth();
-  const [shouldRedirect, setShouldRedirect] = useState(false);
 
-  useEffect(() => {
-    if (!isAuthenticated) {
-      toast.error("Du måste logga in för att se denna sidan");
-      setShouldRedirect(true);
-    }
-  }, [isAuthenticated]);
-
-  if (shouldRedirect) {
+  if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
 
