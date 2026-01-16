842612469997bf78d8b3798d3dfa8af5a4d0922f
annappersson <annasigridmolly@gmail.com>
2026-01-16 09:13:04 +0100

    feat: add getCities function to demoAPI service for fetching city data

--- demo/src/services/demoAPI.ts
+++ demo/src/services/demoAPI.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
 
+export interface CitiesResponse {
+  cities: {
+    guid: string;
+    name: string;
+  };
+}
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("access_token") || ""}`,
-    },
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("access_token") || ""}`,
+  },
 });
+
+const get = async <T>(endpoint: string) => {
+  const res = await api.get<T>(endpoint);
+
+  return res.data;
+};
+
+export const getCities = () => {
+  return get<CitiesResponse>(`/cities`);
+};
 
 export default api;
